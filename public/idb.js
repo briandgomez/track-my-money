c01b416965febe44880c91405796041170fa3d42
briandgomez <briandgomez98@gmail.com>
2021-12-28 21:07:11 -0500

    connect to indexedDB

---
+++ public/idb.js
@@ -0,0 +1,34 @@
+let db;
+
+//Connect & create database 'budget_db'version 1
+const request = indexedDB.open('budget_db', 1);
+
+//Event listener to update the data if there is a change of the database
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    //Store 'new_transaction' table in database
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+}
+
+//If connection to the database is successful, save the object to 'db'
+request.onsuccess = function (event) {
+    db = event.target.result;
+    //If app is online run uplaodTransaction function
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+//If connection to database is unsuccessful log the error
+request.onerror = function (event) {
+    console.log(event.target.errorCode)
+};
+
+function saveRecord(record) {
+    //Open a new transaction using the 'budget_db' table with read and write permissions
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    //Access 'new_transaction' object store
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    //Add record to table
+    transactionObjectStore.add(record);
+};
