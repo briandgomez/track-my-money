939bab786f3ff3a767ffad2429c6254a855ec73a
briandgomez <briandgomez98@gmail.com>
2021-12-30 21:47:08 -0500

    added index.js to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,8 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "./index.html",
-    "./css/styles.css"
+    "./css/styles.css",
+    "./js/index.js"
 ];
 
 //Install service worker
