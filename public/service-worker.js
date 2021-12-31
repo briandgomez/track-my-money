8fd9f0253dd57365f46bf8f1706c883e24307e3c
briandgomez <briandgomez98@gmail.com>
2021-12-30 20:56:30 -0500

    moved service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,8 +2,8 @@ const APP_PREFIX = 'Budget Tracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "./public/index.html",
-    "./public/css/styles.css"
+    "./index.html",
+    "./css/styles.css"
 ];
 
 //Install service worker
