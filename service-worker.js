42987c1389178822210159e25a50136d3c62dbc2
briandgomez <briandgomez98@gmail.com>
2021-12-30 19:30:51 -0500

    fixed register service worker

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const APP_PREFIX = 'Budget Tracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "./index.html",
+    "./public/index.html",
     "./public/css/styles.css"
 ];
 
